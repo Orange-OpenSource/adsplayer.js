08dc1e4906962e9adfae652e0ff9d4158c7852fe
Nicolas ANGOT <nicolas.angot@orange.com>
2016-04-28 15:20:52 +0200

    update gulp build

--- build_gulp/gulpfile.js
+++ build_gulp/gulpfile.js
@@ -1,27 +1,60 @@
 // Requis
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
-var del = require('del');
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')(),
+	del = require('del'),
+	git = require('git-rev'),
+	runSequence = require('run-sequence'),
+	fs = require("fs");
+	
+var pkg = { revision : '',
+			timeStamp : '',
+			licence : ''};
 
-// Variables de chemins
-var source = '../src'; // dossier de travail
-var destination = '../dist'; // dossier à livrer
+var comment = '<%= pkg.licence %>\n\n/* Last build : <%= pkg.timeStamp %> / git revision : <%= pkg.revision %> */\n\n';
 
+var path = {
+  JS: ['../src/**/*.js'],
+  MINIFIED_OUT: 'adsplayer.min.js',
+  CONCAT_OUT: 'adsplayer.js',
+  DEST: '../dist',
+  LICENCE : '../LICENSE'
+};
  
 gulp.task('lint', function() {
-  return gulp.src(source+'/**/*.js')
+  return gulp.src(path.JS)
     .pipe($.jshint())
     .pipe($.jshint.reporter('jshint-stylish'));
 });
  
+ gulp.task('gitRev', function() {
+	git.short(function (str) {
+		pkg.revision = str;
+	})
+	fs.readFile(path.LICENCE, null, function(err, _data) {
+		console.log('read licence file ');
+		pkg.licence = _data;
+    })
+	pkg.timeStamp = new Date().getDate()+"."+(new Date().getMonth()+1)+"."+(new Date().getFullYear())+"_"+(new Date().getHours())+":"+(new Date().getMinutes())+":"+(new Date().getSeconds());
+ });
+ 
 gulp.task('compress', function() {
-  return gulp.src(source+'/**/*.js')
-    .pipe(uglify())
-    .pipe(gulp.dest(destination));
+	return gulp.src(path.JS)
+    .pipe($.concat(path.CONCAT_OUT))
+	.pipe(gulp.dest(path.DEST))
+    .pipe($.uglify())
+	.pipe($.banner(comment, {
+		pkg: pkg
+	}))
+	.pipe($.rename(path.MINIFIED_OUT))
+    .pipe(gulp.dest(path.DEST));
 });
 
 // Clean Output Directory
-gulp.task('clean', del.bind(null, [destination], {
+gulp.task('clean', del.bind(null, [path.DEST], {
     dot: true,
 	force: true
 }));
+
+gulp.task('build', function() {
+	runSequence('clean',['lint', 'gitRev'],'compress');
+});
