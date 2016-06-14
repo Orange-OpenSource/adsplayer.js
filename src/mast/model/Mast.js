96e7711dda1a79bf55a33d4753a57ac78225fb18
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-06-14 15:34:02 +0200

    Mast.js:update model

--- src/mast/model/Mast.js
+++ src/mast/model/Mast.js
@@ -1,16 +1,3 @@
-/*
- * The copyright in this software is being made available under the BSD License, included below. This software may be subject to other third party and contributor rights, including patent rights, and no such rights are granted under this license.
- *
- * Copyright (c) 2016, Orange
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
- * •  Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
- * •  Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
- * •  Neither the name of the Digital Primates nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
 
 // ConditionType
 var ConditionType = {
@@ -29,13 +16,13 @@ var ConditionName = {
 
 // ConditionOperator
 var ConditionOperator = {
-    EQ: 'EQ',
-    NEQ: 'NEQ',
-    GTR: 'GTR',
-    GEQ: 'GEQ',
-    LT: 'LT',
-    LEQ: 'LEQ',
-    MOD: 'MOD'
+    EQ:  'EQ',  // equal
+    NEQ: 'NEQ', // not equal
+    GTR: 'GTR', // greater
+    GEQ: 'GEQ', // greater or equal
+    LT:  'LT',  // lower
+    LEQ: 'LEQ', // lower or equal
+    MOD: 'MOD'  // modulo
 };
 
 // SourceFormat
@@ -50,7 +37,6 @@ var SourceFormat = {
 AdsPlayer.mast.model.Mast = function () {
     "use strict";
     this.triggers = [];     // pointer to any number of Ad objects
-
 };
 
 /**
@@ -59,6 +45,7 @@ AdsPlayer.mast.model.Mast = function () {
 AdsPlayer.mast.model.Trigger = function() {
     "use strict";
 
+    this.baseUrl = '';
     this.id = '';
     this.description = 0;
     this.startConditions = []; // pointer to a list of start conditions to  : AdsPlayer.mast.model.Trigger.Condition
@@ -66,6 +53,7 @@ AdsPlayer.mast.model.Trigger = function() {
     this.sources = []; // pointer to a list of sources : AdsPlayer.mast.model.Trigger.Source
     this.alreadyProcessed = false; // mainly in the seeked case : do not replay trigger already played
     this.ads = [];
+    this.vasts = [];
 };
 
 AdsPlayer.mast.model.Trigger.prototype = {
