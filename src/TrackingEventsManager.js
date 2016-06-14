e7535090a7012ed29e10700ed44b8b05be9668be
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-06-14 15:44:54 +0200

    TrackingEventsManager.js: on prgress...

--- src/TrackingEventsManager.js
+++ src/TrackingEventsManager.js
@@ -16,8 +16,72 @@ AdsPlayer.TrackingEventsManager = function() {
         _debug = AdsPlayer.Debug.getInstance(),
         _eventBus = AdsPlayer.EventBus.getInstance(),
 
+        _postEvent = function (uri) {
+            if (uri === "") {
+                return;
+            }
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', uri, true);
+            xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+            xhr.send();
+        },
+
+        _addEventListener = function (element, type, callback, once) {
+            var _cb = function() {
+                if (once && typeof once === "boolean") {
+                    element.removeEventListener(type, _cb, false);
+                }
+                callback();
+            };
+            element.addEventListener(type, _cb, false);
+            _listeners.push({
+                element: element,
+                type: type,
+                callback: _cb
+            });
+
+        },
 
         _listenPlayerEvents = function () {
+
+
+            for (i = 0; i < _trackingEvents.length; i++) {
+                trackingEvent = _trackingEvents[i];
+                switch (trackingEvent.event) {
+                    case 'start':
+                        _addEventListener(_adMediaPlayer, 'playing', _postFunction(trackingEvent.uri), true);
+                        break;
+                    case 'complete':
+                        _addEventListener(_adMediaPlayer, 'ended', _postFunction(trackingEvent.uri, true));
+                        break;
+                    case 'creativeView':
+                        _addEventListener(_adMediaPlayer, 'loadeddata', _postFunction(trackingEvent.uri), true);
+                        break;
+                    case 'firstQuartile':
+                    case 'thirdQuartile':
+                    case 'midpoint':
+                        myEvent = new AdsPlayer.AdsTrackingEvents.ProgressEvent();
+                        myEvent.trackingEvent = trackingEvent;
+                        _trackingProgressEvents.push(myEvent);
+                        timeUpdateFlag = true;
+                        break;
+                    case 'mute':
+                        uriMute = trackingEvent.uri;
+                        break;
+                    case 'unmute':
+                        uriUnmute = trackingEvent.uri;
+                        break;
+                    case 'pause':
+                        _addEventListener(_adMediaPlayer, 'pause', _CB_Pause(trackingEvent.uri));
+                        break;
+                    case 'resume':
+                        _addEventListener(_adMediaPlayer, 'play', _CB_Resume(trackingEvent.uri));
+                        break;
+                    case 'fullscreen':
+                        _addFullScreenListener(trackingEvent.uri);
+                        break;
+                }
+            }
 
         };
 
