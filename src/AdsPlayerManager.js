27740fd51a8e106b41e512f5a9e56580e688c67e
Bertrand Berthelot <bertrand.berthelot@orange.com>
2016-06-21 16:01:05 +0200

    AdsPlayerManager.js: implement error use cases

--- src/AdsPlayerManager.js
+++ src/AdsPlayerManager.js
@@ -10,6 +10,7 @@
 AdsPlayer.AdsPlayerManager = function() {
 
     var _vasts = [],
+        _adPlayerContainer = null,
         _adPlayer = null,
         _vastIndex = -1,
         _creativeIndex = -1,
@@ -19,20 +20,48 @@ AdsPlayer.AdsPlayerManager = function() {
         _mediaPlayer = null,
         _trackingEventsManager = null,
 
-        _onMediaEnded = function () {
-            // Stop the MediaPlayer
-            _mediaPlayer.stop();
-            _mediaPlayer.reset();
-            _mediaPlayer = null;
+        _stopCreative = function () {
+            // Stop the media player
+            if (_mediaPlayer) {
+                _mediaPlayer.stop();
+                _mediaPlayer.removeEventListener('error', _onMediaError);
+                _mediaPlayer.removeEventListener('ended', _onMediaEnded);
+                _mediaPlayer.reset();
+                _mediaPlayer = null;
+            }
 
             // Stop the TrackingEvents manager
             if (_trackingEventsManager) {
                 _trackingEventsManager.stop();
                 _trackingEventsManager = null;
             }
+        },
+
+        _onMediaError = function () {
+
+            _debug.log("Media error");
+
+            // Stop the current creative media
+            _stopCreative();
 
             // Play next creative
             _playNextCreative();
+        },
+
+        _onMediaEnded = function () {
+
+            _debug.log("Media ended");
+
+            // Stop the current creative media
+            _stopCreative();
+
+            // Play next creative
+            _playNextCreative();
+        },
+
+        _showMediaPlayer = function () {
+            _adPlayerContainer.appendChild(_mediaPlayer.getElement());
+            _mediaPlayer.getElement().style.visibility = 'visible';
         },
 
         _playMedia = function (baseUrl, mediaFiles, trackingEvents) {
@@ -41,7 +70,7 @@ AdsPlayer.AdsPlayerManager = function() {
                 isImage;
 
             if (mediaFiles.length === 0) {
-                return;
+                return false;
             }
 
             mediaFile = mediaFiles[0];
@@ -53,42 +82,40 @@ AdsPlayer.AdsPlayerManager = function() {
             if (isVideo) {
                 _mediaPlayer = new AdVideoPlayer();
             }
-            if (isImage) {
+            else if (isImage) {
                 //_mediaPlayer = new AdImagePlayer();
+            } else {
+                // Unknown/unsupported media type
+                return false;
             }
 
+            // Load the media files
+            if (!_mediaPlayer.load(baseUrl, mediaFiles)) {
+                return false;
+            }
+            // Notify an ad is starting to play
+            _eventBus.dispatchEvent({
+                type: "adMediaStart",
+                data: {}
+            });
+
+            _mediaPlayer.addEventListener('error', _onMediaError);
+            _mediaPlayer.addEventListener('ended', _onMediaEnded);
+
+            // Show the media player 
+            _showMediaPlayer();
+
+            // Add tracking events
             if (trackingEvents) {
                 _trackingEventsManager = new AdsPlayer.TrackingEventsManager();
                 _trackingEventsManager.init(trackingEvents, _mediaPlayer);
                 _trackingEventsManager.start();
             }
 
-            // Load the media files
-            if (_mediaPlayer.load(baseUrl, mediaFiles)) {
-                // Notify an ad is starting to play
-                _eventBus.dispatchEvent({
-                    type: "adMediaStart",
-                    data: {}
-                });
+            // Start playing the media
+            _mediaPlayer.play();
 
-                _mediaPlayer.addEventListener('ended', _onMediaEnded);
-
-                _mediaPlayer.play();
-            }
-
-            /*var mediaFilesSupported = [];
-
-            // We filter the unsupported media files
-
-            // First, we sort the mediafiles in bitrate ascending order
-            mediaFiles.sort(function(a, b) {
-                if (a.bitrate && b.bitrate) {
-                    return a.bitrate - b.bitrate;
-                }
-                return -1;
-            });*/
-
-            // We play the media file with lowest bitrate
+            return true;
         },
 
         _sendImpression = function (impression) {
@@ -109,8 +136,10 @@ AdsPlayer.AdsPlayerManager = function() {
 
             // Play Linear element
             linear = creative.linear;
-            _debug.log("Play Linear Ad");
-            _playMedia(_vasts[_vastIndex].baseUrl, linear.mediaFiles, linear.trackingEvents);
+            _debug.log("Play Linear Ad, duration = " + linear.duration);
+            if (!_playMedia(_vasts[_vastIndex].baseUrl, linear.mediaFiles, linear.trackingEvents)) {
+                _playNextCreative();
+            }
         },
 
         _playAd = function () {
@@ -140,7 +169,7 @@ AdsPlayer.AdsPlayerManager = function() {
 
             _vastIndex++;
 
-            if (_vastIndex < vasts.length) {
+            if (_vastIndex < _vasts.length) {
                 _playVast(_vastIndex);
             } else {
                 // Notify end of trigger
@@ -156,13 +185,12 @@ AdsPlayer.AdsPlayerManager = function() {
         },
 
         _playNextCreative = function () {
-            var vast = _vasts[_vastIndex],
-                ad = vasts[_vastIndex].ads[_adIndex];
+            var ad = _vasts[_vastIndex].ad;
 
             _creativeIndex++;
 
             if (_creativeIndex < ad.inLine.creatives.length) {
-                _playCreative(ad.inLine.creatives[_creativeIndex]);
+                _playCreative(_creativeIndex);
             } else {
                 _playNextAd();
             }
@@ -181,15 +209,16 @@ AdsPlayer.AdsPlayerManager = function() {
          * @memberof AdsPlayerManager#
          * @param {Array} vasts - the array of Vast components to play
          */        
-        init: function(vasts) {
+        init: function(vasts, adPlayerContainer) {
             _vasts = vasts;
+            _adPlayerContainer = adPlayerContainer;
         },
 
         start: function() {
             if (_vasts.length === 0) {
                 return;
             }
-
+            
             // Notify a trigger is starting to play
             _eventBus.dispatchEvent({
                 type: "adTriggerStart",
@@ -200,7 +229,7 @@ AdsPlayer.AdsPlayerManager = function() {
         },
 
         stop: function() {
-
+            _stopCreative();
         }
 
     };
