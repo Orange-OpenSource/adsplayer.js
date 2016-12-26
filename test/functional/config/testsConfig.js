dd108e75906a7243123c8828a248c04c5d2b9147
Alain Lebreton <alain.lebreton@viaccess-orca.com>
2016-12-26 15:01:49 +0100

    use the right html test page

--- test/functional/config/testsConfig.js
+++ test/functional/config/testsConfig.js
@@ -7,7 +7,7 @@ define(function(require) {
             // Common tests suite configuration fields
             asyncTimeout: 10,                                                                                           // TODO: what it is for?
             adsDuration : 10,                                                                                           // TODO: what it is for?
-            testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/samples/testsPlayer",                        // url of the html page under test
+            testPageUrl : "http://cswebplayer.viaccess.fr/functionnalTests/CSAdsPlugin-Dev/samples/testsPlayer",        // url of the html page under test
             streamUrl   : "http://playready.directtaps.net/smoothstreaming/SSWSS720H264/SuperSpeedway_720.ism/Manifest",// url of the main stream
                                                                                                                         // take care using one with video.currentTime = 0 at the beginning
                                                                                                                         // for the pre-roll tests
